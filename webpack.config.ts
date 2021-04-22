38b1ef0eee1103e6bb9cebe296146d7cf4f2c625
josemanuel.alvarez <josemanuel.alvarez@mirai.com>
2021-04-22 18:47:10 +0200

    PostCSS

--- webpack.config.ts
+++ webpack.config.ts
@@ -24,7 +24,7 @@ const config: Configuration = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(ts|js)x?$/,
