89961177a8bfe4485350b412037e70e5ae538af2
josemanuel.alvarez <josemanuel.alvarez@mirai.com>
2021-04-24 21:59:46 +0200

    PostCSS Webpack 5

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-url'),
-    require('postcss-preset-env')
-  ]
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+      stage: 0
+    }
+  }
 }
