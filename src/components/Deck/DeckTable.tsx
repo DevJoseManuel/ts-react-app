1592eb5a51bf5c0034e505bfbf49be26d7e768cb
josemanuel.alvarez <josemanuel.alvarez@mirai.com>
2021-04-24 14:29:19 +0200

    UX & UI

--- src/components/Deck/DeckTable.tsx
+++ src/components/Deck/DeckTable.tsx
@@ -7,9 +7,11 @@ import styles from './deck-table.module.css'
 import { useSelector } from 'react-redux'
 import { CardState } from '../../store/models/card/types'
 import { Store } from '../../store/redux'
+import { UserReduxUI } from '../../store/models/user/types'
 
 const DeckTable: React.FC = () => {
   const { model, ui } = useSelector<Store, CardState>(state => state.cards)
+  const { totalCards } = useSelector<Store, UserReduxUI>(state => state.user.ui)
 
   const rows = _.isUndefined(ui.filter) ? model.result : ui.filter.results
   const text = _.isUndefined(ui.filter) ? '' : ui.filter.text
@@ -17,7 +19,13 @@ const DeckTable: React.FC = () => {
   return (
     <>
       <div className={styles.navbar}>
-        <h2>Card List</h2>
+        <div>
+          <div className={styles.topBar}>
+            <h2>Card List</h2>
+            <div className={styles.countName}>Total cards</div>
+            <div className={styles.count}>{totalCards}</div>
+          </div>
+        </div>
         <DeckTableFilter text={text} />
       </div>
       <div className={styles.main}>
